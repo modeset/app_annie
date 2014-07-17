d9e98f0f6be1bbfb9b6685bfd6769b1eb8aed1db
Dylan Jhaveri <dylanjhaveri@gmail.com>
2014-07-16 17:32:39 -0700

    create AppAnnie::ErrorReponse class
    * remove some code duplication with error handling
    * AppAnnie::ErrorResponse.raise_for(response) will handle
    all non-200 errors.

--- lib/app_annie.rb
+++ lib/app_annie.rb
@@ -1,5 +1,6 @@
 require 'faraday'
 require 'json'
+require 'app_annie/error_response'
 require 'app_annie/account'
 require 'app_annie/app'
 require 'app_annie/intelligence'
@@ -24,13 +25,10 @@ module AppAnnie
       req.url '/v1/accounts', options
     end
 
-    case response.status
-    when 200 then return JSON.parse(response.body)['account_list'].map { |hash| Account.new(hash) }
-    when 401 then raise Unauthorized, "Invalid API key - set an env var for APPANNIE_API_KEY or set AppAnnie.api_key manually"
-    when 429 then raise RateLimitExceeded
-    when 500 then raise ServerError
-    when 503 then raise ServerUnavailable
-    else raise BadResponse, "An error occurred. Response code: #{response.status}"
+    if response.status == 200
+      JSON.parse(response.body)['account_list'].map { |hash| Account.new(hash) }
+    else
+      ErrorResponse.raise_for(response)
     end
   end
 
--- lib/app_annie/account.rb
+++ lib/app_annie/account.rb
@@ -28,13 +28,10 @@ module AppAnnie
         req.url "/v1/accounts/#{id}/apps", options
       end
 
-      case response.status
-      when 200 then return JSON.parse(response.body)['app_list'].map { |hash| App.new(self, hash) }
-      when 401 then raise Unauthorized, "Invalid API key - set an env var for APPANNIE_API_KEY or set AppAnnie.api_key manually"
-      when 429 then raise RateLimitExceeded
-      when 500 then raise ServerError
-      when 503 then raise ServerUnavailable
-      else raise BadResponse, "An error occurred. Response code: #{response.status}"
+      if response.status == 200
+        JSON.parse(response.body)['app_list'].map { |hash| App.new(self, hash) }
+      else
+        ErrorResponse.raise_for(response)
       end
     end
 
@@ -45,13 +42,10 @@ module AppAnnie
         req.url "/v1/accounts/#{id}/sales", options
       end
 
-      case response.status
-      when 200 then return JSON.parse(response.body)['sales_list']
-      when 401 then raise Unauthorized, "Invalid API key - set an env var for APPANNIE_API_KEY or set AppAnnie.api_key manually"
-      when 429 then raise RateLimitExceeded
-      when 500 then raise ServerError
-      when 503 then raise ServerUnavailable
-      else raise BadResponse, "An error occurred. Response code: #{response.status}"
+      if response.status == 200
+        JSON.parse(response.body)['sales_list']
+      else
+        ErrorResponse.raise_for(response)
       end
     end
 
