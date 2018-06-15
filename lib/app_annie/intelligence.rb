b02afc0ddbc8ae19b1f2c108c4b66300e68d42a6
Todd Morningstar <todd@thoughtbot.com>
2018-06-14 19:11:12 -0700

    Add Interface for Usage-History Endpoint

--- lib/app_annie/intelligence.rb
+++ lib/app_annie/intelligence.rb
@@ -36,6 +36,40 @@ module AppAnnie
       end
     end
 
+    def self.app_usage_history(opts)
+      [
+        :app_id,
+        :countries,
+        :market,
+      ].each do |key|
+        unless opts.keys.include?(key)
+          raise ArgumentError, "Missing #{key} key in options"
+        end
+      end
+
+      default_options = {
+        vertical: "apps",
+        device: "all",
+      }
+      opts = default_options.merge(opts)
+
+      response = AppAnnie.connection.get do |req|
+        req.headers["Authorization"] = "Bearer #{AppAnnie.api_key}"
+        req.headers["Accept"] = "application/json"
+        req.url(
+          "#{API_ROOT}/#{opts[:vertical]}/#{opts[:market]}/app/"\
+            "#{opts[:app_id]}/usage-history"
+        )
+        req.params = opts
+      end
+
+      if response.status == 200
+        JSON.parse(response.body)
+      else
+        ErrorResponse.raise_for(response)
+      end
+    end
+
     # see AppAnnie Intelligence docs for options
     # all ops get passed as query params
     def self.top_app_charts(opts)
--- spec/app_annie/intelligence_spec.rb
+++ spec/app_annie/intelligence_spec.rb
@@ -38,18 +38,61 @@ describe AppAnnie::Intelligence do
       end
     end
 
-    def stub_connection(path, payload)
-      Faraday.new do |builder|
-        builder.adapter :test do |stub|
-          stub.get(path) { [200, {}, payload] }
-        end
-      end
-    end
-
     def app_history_payload
       File.read(
         File.expand_path(
           "../../api_fixtures/intelligence_app_history.json",
+          __FILE__,
+        ),
+      )
+    end
+  end
+
+  describe ".app_usage_history" do
+    describe "make an api query" do
+      describe "with missing options" do
+        it "should throw an error" do
+          expect { AppAnnie::Intelligence.app_usage_history({}) }.
+            to raise_error(ArgumentError)
+        end
+      end
+
+      describe "successfully" do
+        it "returns results" do
+          options = {
+            app_id: "54321",
+            device: "iphone",
+            countries: "WW",
+            granularity: "weekly",
+            market: "ios",
+            start_date: "2018-05-27",
+          }
+          path = "/v1.2/intelligence/apps/#{options[:market]}/app/"\
+            "#{options[:app_id]}/usage-history?countries=#{options[:countries]}"\
+            "&device=#{options[:device]}&granularity=#{options[:granularity]}"\
+            "&start_date=#{options[:start_date]}"
+          stubbed_connection = stub_connection(path, app_usage_history_payload)
+          allow(AppAnnie).to receive(:connection).and_return(stubbed_connection)
+
+          result = AppAnnie::Intelligence.app_usage_history(options)
+
+          expect(result).to match(a_hash_including({
+            "vertical" => "apps",
+            "market" => "ios",
+            "code" => 200,
+            "product_id" => 54321,
+            "product_name" => "ExampleCompanyApp",
+            "granularity" => "weekly",
+            "list" => kind_of(Array),
+          }))
+        end
+      end
+    end
+
+    def app_usage_history_payload
+      File.read(
+        File.expand_path(
+          "../../api_fixtures/intelligence_app_usage_history.json",
           __FILE__,
         ),
       )
@@ -80,6 +123,14 @@ describe AppAnnie::Intelligence do
         it 'should return results' do
           expect(AppAnnie::Intelligence.top_app_charts(options)).to be_an(Array)
         end
+      end
+    end
+  end
+
+  def stub_connection(path, payload)
+    Faraday.new do |builder|
+      builder.adapter :test do |stub|
+        stub.get(path) { [200, {}, payload] }
       end
     end
   end
